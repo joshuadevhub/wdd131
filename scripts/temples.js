12c5dbc82d9ced5c3b692ce9114b9c87edb5d057
joshuadevhub <elemide.j.dev@gmail.com>
2025-07-12 00:57:31 +0100

    some changes made

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,6 +1,14 @@
 const buttonToggle = document.getElementById("menu");
 const navMenu = document.querySelector(".navigation");
 const header = document.querySelector("header");
+const fullYear = document.querySelector("#currentyear");
+const lastmodified = document.querySelector("#lastmodified")
+
+const today = new Date();
+
+fullYear.innerHTML = today.getFullYear()
+lastmodified.innerHTML = `Last Modified: ${document.lastModified}`
+
 
 buttonToggle.addEventListener("click", () => {
   buttonToggle.classList.toggle("open");
